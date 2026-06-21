2e0c5b2aa562c12be31a263c4f223fef1921bd27
shankarcoder23 <shankar.coder23@gmail.com>
2026-06-21 20:31:47 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("Naba Siksha School Website Loaded");
 
 });
+
+document.querySelector('.custom-toggler').addEventListener('click', function () {
+    this.classList.toggle('active');
+});
