86a00effbcc5fb1811e5a930a67f4cfe7bf17d20
shankarcoder23 <shankar.coder23@gmail.com>
2026-06-22 17:16:53 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,45 @@ toggleBtn.addEventListener("click", () => {
     socialMenu.classList.toggle("show");
 
 });
+
+ 
+
+ /* Notice */  
+
+        const filterBtns = document.querySelectorAll('.filter-btn');
+        const noticeItems = document.querySelectorAll('.notice-item');
+
+        filterBtns.forEach(btn => {
+
+            btn.addEventListener('click', () => {
+
+                filterBtns.forEach(b =>
+                    b.classList.remove('active'));
+
+                btn.classList.add('active');
+
+                const filter =
+                    btn.dataset.filter;
+
+                noticeItems.forEach(item => {
+
+                    if (
+                        filter === 'all' ||
+                        item.classList.contains(filter)
+                    ) {
+
+                        item.style.display = 'block';
+
+                    } else {
+
+                        item.style.display = 'none';
+
+                    }
+
+                });
+
+            });
+
+        });
+
+  
